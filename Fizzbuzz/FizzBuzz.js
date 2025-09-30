743a4625a9cfde196be06caa85784a9e0e03dee9
Meiqi Wu <aliciadaniswu@gmail.com>
2025-09-30 10:33:29 -0400

    fizzbuzz project

---
+++ Fizzbuzz/FizzBuzz.js
@@ -0,0 +1,36 @@
+
+
+/*
+
+let num = 0
+
+while (num <= 100) {
+	num ++;
+	if (num % 3 === 0 && num % 5 === 0) {
+	console.log ("FizzBuzz");
+		} else if (num % 5 === 0){
+			console.log ("Buzz");
+		} else if (num % 3 === 0) {
+			console.log("Fizz");
+		} else {
+			console.log(num);
+		}
+
+}
+
+*/
+
+/*faster way*/
+
+for (let num = 1; num <= 100; num++) {
+	if (num % 3 === 0 && num % 5 === 0) {
+	console.log ("FizzBuzz");
+		} else if (num % 5 === 0){
+			console.log ("Buzz");
+		} else if (num % 3 === 0) {
+			console.log("Fizz");
+		} else {
+			console.log(num);
+		}
+}
+
